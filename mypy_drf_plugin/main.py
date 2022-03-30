ca035c8a7d95b863f1474dd7f923877e433e66be
Sondre Lillebø Gundersen <sondrelg@live.no>
2022-03-30 14:33:34 +0200

    fix: Load django_settings_module from mypy config file

--- mypy_drf_plugin/main.py
+++ mypy_drf_plugin/main.py
@@ -1,9 +1,9 @@
+from configparser import ConfigParser
 from typing import Callable, Dict, Optional
 
 from mypy.nodes import TypeInfo
 from mypy.options import Options
 from mypy.plugin import ClassDefContext, Plugin
-from mypy_django_plugin import main as mypy_django_main
 from mypy_django_plugin.django.context import DjangoContext
 
 from mypy_drf_plugin.lib import fullnames, helpers
@@ -22,7 +22,25 @@ class NewSemanalDRFPlugin(Plugin):
     def __init__(self, options: Options) -> None:
         super().__init__(options)
 
-        django_settings_module = mypy_django_main.extract_django_settings_module(options.config_file)
+        if not self.options.config_file:
+            raise ValueError("Missing config file")
+
+        config = ConfigParser()
+        config.read(self.options.config_file)
+
+        if "mypy.plugins.django-stubs" not in config:
+            raise ValueError("Missing `mypy.plugins.django-stubs` section in the mypy config file.")
+
+        elif "django_settings_module" not in config["mypy.plugins.django-stubs"]:
+            raise ValueError(
+                "Missing `django_settings_module` in the "
+                "`mypy.plugins.django-stubs` section of the mypy config file."
+            )
+        else:
+            django_settings_module = (
+                config["mypy.plugins.django-stubs"]["django_settings_module"].lstrip('"').rstrip('"')
+            )
+
         self.django_context = DjangoContext(django_settings_module)
 
     def _get_currently_defined_serializers(self) -> Dict[str, int]:
